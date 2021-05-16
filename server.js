058b4fa28a14584a00df8523ff9ea511c7ff3604
sanamahuja <75626567+sanamahuja@users.noreply.github.com>
2021-05-17 03:47:32 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyparser = require("body-parser");
+
+const app =express();
+app.use(bodyparser.urlencoded({extended: true}));
+
+app.get("/",function(req,res)
+{res.sendFile(__dirname+"/index.html");});
+app.post("/",function(req,res)
+{ var n1=Number(req.body.num1);
+  var n2=Number(req.body.num2);
+  var result=n1/(n2*n2);
+    res.send("this is answer" + " "+result);});
+
+app.listen(3000,function()
+{console.log("welcome to the the server");});
